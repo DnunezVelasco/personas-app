e262d568325e08b4fbef7b9dc08911567fdfb58b
David Nuñez Velasco <130922649+DnunezVelasco@users.noreply.github.com>
2023-10-30 20:31:23 -0500

    Commune Add

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -24,10 +24,48 @@ class ComunaController extends Controller
            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
            ->get();
 
-        //dd($comunas);
+        //dd($comunas); 
 
         return view("comunas.index", ["comunas" => $comunas]);
     }
 
+   /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+
+
+        return view('comunas.new', [
+            'municipios' => $municipios,
+
+        ]);
+    }
+
+        /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $comuna = new Comuna();
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+        return redirect()->route('comunas.index') ;
+
+     $comunas = DB::table('tb_comuna')
+           ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+           ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+           ->get();
+    }
 
 }
