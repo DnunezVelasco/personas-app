4e4219c417d4d01322f217da2bf971c2ebdeb0cd
David Nuñez Velasco <130922649+DnunezVelasco@users.noreply.github.com>
2023-10-31 00:07:09 -0500

    lista municipios

---
+++ app/Http/Controllers/MunicipioController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Municipio;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MunicipioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $municipios = Municipio::with('departamento')->get();
+        return view('municipios.index', ['municipios' => $municipios]);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $departamentos = DB::table('tb_departamento')
+            ->orderBy('depa_nomb')
+            ->get();
+        return view('municipios.create', ['departamentos' => $departamentos]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Municipio::create([
+            'muni_nomb' => $request->name,
+            'depa_codi' =>$request->departamento
+        ]);
+
+        session()->flash('mensaje', 'Se ha creado un nuevo registro correctamente.');
+        return redirect()->route('municipios.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($muni_codi)
+    {
+        $municipio = Municipio::find($muni_codi);
+        $departamentos = DB::table('tb_departamento')
+            ->orderBy('depa_nomb')
+            ->get();
+        return view('municipios.edit', ['municipio' => $municipio, 'departamentos' => $departamentos]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $muni_codi)
+    {
+        $municipio = municipio::find($muni_codi);
+        $municipio->muni_nomb = $request->name;
+        $municipio->muni_codi = $request->municipio;
+        $municipio->depa_codi = $request->departamento;
+        $municipio->save();
+
+        $municipios = DB::table('tb_municipio')
+            ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+            ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+            ->get();
+        session()->flash('mensaje', 'El campo se actualizo correctamente.');
+        return view('municipios.index', ['municipios' => $municipios]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($muni_codi)
+    {
+        $municipio = Municipio::find($muni_codi);
+        $municipio->delete();
+
+        $municipios = DB::table('tb_municipio')
+            ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+            ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+            ->get();
+        session()->flash('mensaje', 'El campo se elimino correctamente.');
+        return view('municipios.index', ['municipios' => $municipios]);
+    }
+}
---
+++ resources/views/municipios/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.min.css" rel="stylesheet">
+    <title>Comunas</title>
+</head>
+
+<body>
+    <div class="container mt-5">
+
+        <div class="mb-4">
+            <h2>Lista municipios</h2>
+        </div>
+
+        <!-- Add New Button -->
+
+        <!-- Table -->
+        <table class="table table-bordered">
+            <thead class="table-dark">
+                <tr>
+                    <th>Codigo</th>
+                    <th>Municipio</th>
+                    <th>Departamento</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($municipios as $municipio)
+                    <tr>
+                        <td>{{ $municipio->muni_codi }}</td>
+                        <td>{{$municipio->muni_nomb}}</td>
+                        <td>{{ $municipio->departamento->depa_nomb ?? 'No disponible' }}</td>
+                        <td>
+
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
