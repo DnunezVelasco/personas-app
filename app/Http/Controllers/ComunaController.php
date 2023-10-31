10c5842b9d9a4fde20ad9b24acc5e9c252462d51
David Nuñez Velasco <130922649+DnunezVelasco@users.noreply.github.com>
2023-10-30 20:17:21 -0500

    Ajsutes de la vista del proyecto

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -17,12 +17,12 @@ class ComunaController extends Controller
      */
     public function index()
     {
-        $comunas = Comuna::all();
-        // $comunas = DB::table('tb_comuna')
-        //     ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
-        //     ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
-        //     ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
-        //     ->get();
+     //   $comunas = Comuna::all();
+        $comunas = DB::table('tb_comuna')
+           ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+           ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+           ->get();
 
         //dd($comunas);
 
